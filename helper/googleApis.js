fa7f90fae5bf7ff4955d91b8ae2e7883ab62bbb7
Ichika354 <handa82624@gmail.com>
2024-11-10 23:02:12 +0700

    Change url

--- helper/googleApis.js
+++ helper/googleApis.js
@@ -8,7 +8,7 @@ const oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, proces
 const scopes = ["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"];
 
 const authorizationUrl = oauth2Client.generateAuthUrl({
-  access_type: "online",
+  access_type: "offline",
   scope: scopes,
   include_granted_scopes: true,
 });
