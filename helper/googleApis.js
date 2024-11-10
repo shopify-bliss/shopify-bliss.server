ee3fb5d5b2a74ae46b8a75792d2abdbc5aa06d0b
Ichika354 <handa82624@gmail.com>
2024-11-10 22:57:20 +0700

    Change url

--- helper/googleApis.js
+++ helper/googleApis.js
@@ -3,12 +3,12 @@ import { google } from "googleapis";
 
 dotenv.config();
 
-const oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, "http://localhost:3000/auth/google/callback");
+const oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, "https://shopify-blissserver.vercel.app/auth/google/callback");
 
 const scopes = ["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"];
 
 const authorizationUrl = oauth2Client.generateAuthUrl({
-  access_type: "offline",
+  access_type: "online",
   scope: scopes,
   include_granted_scopes: true,
 });
