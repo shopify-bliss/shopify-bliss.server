b65388a4fcaccbe621371a3d307dcad26ca17dda
Ichika354 <handa82624@gmail.com>
2024-11-18 14:23:56 +0700

    Configure middleware in the main application setup

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ import verify from "./controller/auth/verifyEmail.js";
 import login from "./controller/auth/login.js";
 import typeTemplate from "./controller/type_templates/type_templates.js";
 
+import configureMiddleware from "./config/middleware.js";
+
 dotenv.config();
 
 const port = 3000;
 const app = express();
+configureMiddleware(app);
 app.use(cookieParser());
 
 app.use(express.json());
